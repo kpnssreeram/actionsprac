e92786eea0f5c6d09efd374d9293718210d618f7
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-04-20 15:48:48 +0530

    Add git checkout

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import boto3
 
-# Create an S3 client
 s3_client = boto3.client('s3')
 
-# List all buckets
 response = s3_client.list_buckets()
 
-# Print bucket names
 for bucket in response['Buckets']:
     print(bucket['Name'])
