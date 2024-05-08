4018dedf228ec96751fa93b8e47eee982f643f8f
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-05-08 18:37:41 +0530

    Add restart service script

--- ssmcmd.sh
+++ ssmcmd.sh
@@ -53,4 +53,22 @@ Instance="$2"
 
 outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Run echo command" --parameters commands='sudo /usr/local/bin/supervisorctl restart all > test1.txt'  --region $AWS_REGION --output text --query "Command.CommandId")
 executedOutput=$(aws ssm list-command-invocations  --region $AWS_REGION  --command-id "$outputSendCommand" --no-cli-pager --details --output text --query "CommandInvocations[].CommandPlugins[].{Output:Output}")
+while true; do
+    STATUS=$(aws ssm list-command-invocations \
+        --region "$AWS_REGION" \
+        --command-id "$outputSendCommand" \
+        --query 'CommandInvocations[0].Status' \
+        --output text)
+
+    if [ "$STATUS" == "Success" ]; then
+        echo "Command execution successful"
+        break
+    elif [ "$STATUS" == "Failed" ]; then
+        echo "Command execution failed"
+        break
+    else
+        echo "Command status: $STATUS"
+        sleep 5
+    fi
+done
 echo "Command output: $executedOutput"
