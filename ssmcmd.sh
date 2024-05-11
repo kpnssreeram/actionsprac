2c557af1d281469f00282082ca03adc6a8a66ea9
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-05-11 23:50:23 +0530

    Add restart service script

--- ssmcmd.sh
+++ ssmcmd.sh
@@ -50,8 +50,15 @@ Instance="$2"
 # echo "Command output:"
 # echo "$OUTPUT"
 
-
-outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Run echo command" --parameters commands='sudo /usr/local/bin/supervisorctl restart all> ScriptExecLog.txt'  --region $AWS_REGION --output text --query "Command.CommandId")
+outputSendCommand=$(aws ssm send-command \
+  --instance-ids "$Instance" \
+  --document-name "AWS-RunShellScript" \
+  --comment "Restart services" \
+  --parameters commands='sudo /usr/local/bin/supervisorctl restart all > ScriptExecLog.txt && sudo systemctl restart cassandra.service > ScriptExecLog1.txt' \
+  --region $AWS_REGION \
+  --output text \
+  --query "Command.CommandId")
+# outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Run echo command" --parameters commands='sudo /usr/local/bin/supervisorctl restart all > ScriptExecLog.txt'  --region $AWS_REGION --output text --query "Command.CommandId")
 executedOutput=$(aws ssm list-command-invocations  --region $AWS_REGION  --command-id "$outputSendCommand" --no-cli-pager --details --output text --query "CommandInvocations[].CommandPlugins[].{Output:Output}")
 while true; do
     STATUS=$(aws ssm list-command-invocations \
