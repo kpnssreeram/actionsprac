1fbe79bd24d526383d2bd1a87057a8527033c7d8
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-05-13 13:10:29 +0530

    Add restart service script

--- ssmcmd.sh
+++ ssmcmd.sh
@@ -1,7 +1,10 @@
 AWS_REGION="$1"
 Instance="$2"
+max_attempts=20  # Maximum number of attempts before considering it as a failure
+attempt_counter=0
 
-outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Run echo command" --parameters commands='sudo /usr/local/bin/supervisorctl restart all> ScriptExecLog.txt'  --region $AWS_REGION --output text --query "Command.CommandId")
+outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Run echo command" --parameters commands='sudo /usr/local/bin/supervisorctl restart all> ScriptExecLog.txt' --region $AWS_REGION --output text --query "Command.CommandId")
+
 while true; do
     STATUS=$(aws ssm list-command-invocations \
         --region "$AWS_REGION" \
@@ -17,6 +20,11 @@ while true; do
         break
     else
         echo "Command status: $STATUS"
+        attempt_counter=$((attempt_counter+1))
+        if [ "$attempt_counter" -ge "$max_attempts" ]; then
+            echo "Maximum attempts reached. Exiting with failure."
+            break
+        fi
         sleep 5
     fi
 done
