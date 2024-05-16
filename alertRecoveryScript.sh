ab9959377241a36e7624a797ef4cda8afc73d663
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-05-16 17:38:05 +0530

    checkOtherCommand

--- alertRecoveryScript.sh
+++ alertRecoveryScript.sh
@@ -49,7 +49,7 @@ function execute_aws_ssm_command {
 function another_aws_ssm_command {
     local AWS_REGION="$1"
     local Instance="$2"
-    local DocumentName="$3"
+    DocumentName="ddd"
 
     local outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "$DocumentName" --comment "Run custom command" --parameters commands='ls' --region "$AWS_REGION" --output text --query "Command.CommandId")
 
