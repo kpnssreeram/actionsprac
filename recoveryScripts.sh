5775431554eab0f519f9663643a5c150c77a7a78
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-05-21 15:48:24 +0530

    Add ECS status

--- recoveryScripts.sh
+++ recoveryScripts.sh
@@ -4,7 +4,6 @@ function check_command_status {
     local outputSendCommand="$2"
     local max_attempts=9
     local attempt_counter=0
-
     while true; do
         local STATUS=$(aws ssm list-command-invocations \
             --region "$AWS_REGION" \
@@ -30,24 +29,6 @@ function check_command_status {
     done
 }
 
-function check_ecs_service_update {
-    local cluster="$1"
-    local service="$2"
-
-    while true; do
-        local service_status=$(aws ecs describe-services --cluster "$cluster" --services "$service" --query 'services[0].deployments[0].status' --output text)
-        if [ "$service_status" == "PRIMARY" ]; then
-            echo "ECS service update successful"
-            break
-        elif [ "$service_status" == "IN_PROGRESS" ]; then
-            echo "ECS service update status: $service_status"
-        else
-            echo "ECS service update failed with status: $service_status"
-            exit 1
-        fi
-        sleep 10
-    done
-}
 function restartAllServices {
     local AWS_REGION="$1"
     local Instance="$2"
@@ -58,19 +39,16 @@ function restartAllServices {
 function updateEcsService {
     REGION="$1"
     cluster_name_prefix="anon-megabus-app-EcsCluster"
-
-    serviceARN=$(aws ecs list-clusters --region "$REGION" --query "clusterArns[?contains(@, '$cluster_name_prefix')]" --output text | xargs -I{} aws ecs list-services --region "$REGION" --cluster {} --query 'serviceArns[*]' --output text)
-    echo "$serviceARN"
+    serviceARN=$(aws ecs list-clusters --region "$REGION" --query "clusterArns[?contains(@, '$cluster_name_prefix')]" --output text | xargs -I{} aws ecs list-services --region "$REGION" --cluster {} --query 'serviceArns[*]')
     for arn in $serviceARN; do
         cluster_name=$(echo "$arn" | awk -F '/' '{print $2}')
         service_name=$(echo "$arn" | awk -F '/' '{print $3}')
         aws ecs update-service --cluster "$cluster_name" --region "$REGION" --service "$service_name" --force-new-deployment
-    #     if [ $? -eq 0 ]; then
-    #         echo "Service $service_name updated with a new force deployment."
-    #     else
-    #         echo "Failed to update service $service_name"
-    #     fi
-    check_ecs_service_update "$cluster_name" "$service_name"
+        if [ $? -eq 0 ]; then
+            echo "Service $service_name updated with a new force deployment."
+        else
+            echo "Failed to update service $service_name"
+        fi
      done
     
 }
