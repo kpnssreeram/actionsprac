fb1a787e43b1cd859ecd8db9e6d4cab1023693fb
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-06-05 00:11:34 +0530

    add profile

--- recoveryScripts.sh
+++ recoveryScripts.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 function check_command_status {
     local AWS_REGION="$1"
     local outputSendCommand="$2"
+    local AWS_PROFILE="$3"
     local max_attempts=9
     local attempt_counter=0
     while true; do
         local STATUS=$(aws ssm list-command-invocations \
             --region "$AWS_REGION" \
+            --profile "$AWS_PROFILE" \
             --command-id "$outputSendCommand" \
             --query 'CommandInvocations[0].Status' \
             --output text)
@@ -21,7 +24,7 @@ function check_command_status {
             echo "Command status: $STATUS"
             attempt_counter=$((attempt_counter+1))
             if [ "$attempt_counter" -ge "$max_attempts" ]; then
-                echo "Maximum attempts reached. execution may or may not be successful.Please check logs or run this script again."
+                echo "Maximum attempts reached. Execution may or may not be successful. Please check logs or run this script again."
                 break
             fi
             sleep 9
@@ -32,22 +35,24 @@ function check_command_status {
 function restartAllServices {
     local AWS_REGION="$1"
     local Instance="$2"
-    local outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Restart Services" --parameters commands='sudo su - & sudo /usr/local/bin/supervisorctl restart all > ScriptExecLog.txt & systemctl restart cassandra.service > ScriptExecLog1.txt' --region "$AWS_REGION" --output text --query "Command.CommandId")
-    check_command_status "$AWS_REGION" "$outputSendCommand"
+    local AWS_PROFILE="$3"
+    local outputSendCommand=$(aws ssm send-command --instance-ids "$Instance" --document-name "AWS-RunShellScript" --comment "Restart Services" --parameters commands='sudo su - & sudo /usr/local/bin/supervisorctl restart all > ScriptExecLog.txt & systemctl restart cassandra.service > ScriptExecLog1.txt' --region "$AWS_REGION" --profile "$AWS_PROFILE" --output text --query "Command.CommandId")
+    check_command_status "$AWS_REGION" "$outputSendCommand" "$AWS_PROFILE"
 }
 
 function updateEcsService {
-    REGION="$1"
-    ENV="$2"
-    CLUSTER="$3"
-    cluster_name_prefix="$ENV-etl-$CLUSTER-app-EcsCluster"
-    serviceARN=$(aws ecs list-clusters --region "$REGION" --query "clusterArns[?contains(@, '$cluster_name_prefix')]" --output text | xargs -I{} aws ecs list-services --region "$REGION" --cluster {} --query 'serviceArns[*]' --output text)
+    local REGION="$1"
+    local ENV="$2"
+    local CLUSTER="$3"
+    local AWS_PROFILE="$4"
+    local cluster_name_prefix="$ENV-etl-$CLUSTER-app-EcsCluster"
+    local serviceARN=$(aws ecs list-clusters --region "$REGION" --profile "$AWS_PROFILE" --query "clusterArns[?contains(@, '$cluster_name_prefix')]" --output text | xargs -I{} aws ecs list-services --region "$REGION" --profile "$AWS_PROFILE" --cluster {} --query 'serviceArns[*]' --output text)
     
     for arn in $serviceARN; do
-        cluster_name=$(echo "$arn" | awk -F '/' '{print $2}')
-        service_name=$(echo "$arn" | awk -F '/' '{print $3}')
+        local cluster_name=$(echo "$arn" | awk -F '/' '{print $2}')
+        local service_name=$(echo "$arn" | awk -F '/' '{print $3}')
 
-        aws ecs update-service --cluster "$cluster_name" --region "$REGION" --service "$service_name" --force-new-deployment
+        aws ecs update-service --cluster "$cluster_name" --region "$REGION" --profile "$AWS_PROFILE" --service "$service_name" --force-new-deployment
         if [ $? -eq 0 ]; then
             echo "Service $service_name update triggered successfully. Waiting for deployment to complete..."
         else
@@ -56,7 +61,7 @@ function updateEcsService {
         fi
 
         while true; do
-            deployment_status=$(aws ecs describe-services --cluster "$cluster_name" --region "$REGION" --services "$service_name" --query 'services[0].deployments[?status==`PRIMARY`].rolloutState' --output text)
+            local deployment_status=$(aws ecs describe-services --cluster "$cluster_name" --region "$REGION" --profile "$AWS_PROFILE" --services "$service_name" --query 'services[0].deployments[?status==`PRIMARY`].rolloutState' --output text)
             if [ "$deployment_status" == "COMPLETED" ]; then
                 echo "Service $service_name has been updated successfully."
                 break
@@ -76,10 +81,18 @@ AWS_REGION="$2"
 Instance="$3"
 cluster_id="$4"
 env="$5"
+
+# Determine the AWS profile based on the environment
+if [[ "$env" == "ci" || "$env" == "cert" || "$env" == "anon" ]]; then
+    AWS_PROFILE="emodb-nexus-qa"
+else
+    AWS_PROFILE="emodb-nexus-prod"
+fi
+
 if [ "$function_name" == "restartAllServices" ]; then
-    restartAllServices "$AWS_REGION" "$Instance"
+    restartAllServices "$AWS_REGION" "$Instance" "$AWS_PROFILE"
 elif [ "$function_name" == "updateEcsService" ]; then
-    updateEcsService "$AWS_REGION" "$env" "$cluster_id"
+    updateEcsService "$AWS_REGION" "$env" "$cluster_id" "$AWS_PROFILE"
 else
     echo "Invalid function name provided."
 fi
